e5e0ded5bcbeea0acd42ed50d6e2f57baff956f9
SzAndras1 <szbandras2@gmail.com>
2024-01-30 19:59:49 +0100

    Refactor chat ui, add notification

--- gateway/web/gatherings-web/src/app/websocket-messages/websocket-messages.component.ts
+++ gateway/web/gatherings-web/src/app/websocket-messages/websocket-messages.component.ts
@@ -3,23 +3,30 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms";
 import {Message} from "../types";
 import {ChatService} from "../services/chat.service";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatTooltipModule, TooltipPosition} from "@angular/material/tooltip";
+import {MatBadgeModule} from "@angular/material/badge";
 
 @Component({
   selector: 'app-websocket-messages',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatChipsModule, MatBadgeModule, MatTooltipModule],
   templateUrl: './websocket-messages.component.html',
   styleUrl: './websocket-messages.component.scss',
   providers: [ChatService]
 })
 export class WebsocketMessagesComponent implements OnInit {
   text!: string;
-  sentMessages: string[] = [];
-  receivedMessages: string[] = [];
-  senderName: string = "user1";
-  group: string = 'room1';
-  sentMessages2: string[] = [];
-  receivedMessages2: string[] = [];
+  receivedMessages: Message[][] = [];
+  senderName: string = 'user1';
+  group: string = 'group1';
+  groupIndex: number = 0;
+  chipColor: string = 'accent';
+  chatPosition: TooltipPosition = 'before';
+
+  unreadMessageCount: number[] = [0, 0, 0];
+  hidden: boolean[] = [true, true, true];
+  availableGroups: string[] = ['group1', 'group2', 'group3'];
 
   constructor(public socketService: ChatService) {
   }
@@ -27,25 +34,29 @@ export class WebsocketMessagesComponent implements OnInit {
   ngOnInit(): void {
     this.socketService.getMessage()
       .subscribe((data: Message) => {
-        console.log(data);
         this.separateMessages(data);
+        this.handleUnreadMessages(data.groupId);
       });
   }
 
   separateMessages(message: Message): void {
-    console.log(`${message.senderName} ${message.groupId} ${message.text}`);
-    if (message.groupId == 'room1') {
-      if (message.senderName == 'user1') {
-        this.receivedMessages.push(`User ${message.senderName} said: ${message.text} to ${message.groupId} group`);
-      } else {
-        this.sentMessages.push(`User ${message.senderName} said: ${message.text} to ${message.groupId} group`);
-      }
-    } else {
-      if (message.senderName == 'user1') {
-        this.receivedMessages2.push(`User ${message.senderName} said: ${message.text} to ${message.groupId} group`);
-      } else {
-        this.sentMessages2.push(`User ${message.senderName} said: ${message.text} to ${message.groupId} group`);
-      }
+    const length: number = message.groupId.length - 1;
+    const index: number = Number(message.groupId.charAt(length)) - 1;
+
+    if (this.receivedMessages[index] === undefined) {
+      this.receivedMessages[index] = [];
+    }
+
+    this.receivedMessages[index].push(message);
+  }
+
+  handleUnreadMessages(groupName: string): void {
+    const length: number = groupName.length - 1;
+    const index: number = Number(groupName.charAt(length)) - 1;
+
+    if (index !== this.groupIndex) {
+      this.hidden[index] = false;
+      this.unreadMessageCount[index]++;
     }
   }
 
@@ -54,7 +65,13 @@ export class WebsocketMessagesComponent implements OnInit {
     this.socketService.emit(message);
   }
 
-  getReceivedMessages(): void {
-    console.log(this.receivedMessages);
+  changeGroup(groupName: string): void {
+    const length: number = groupName.length - 1;
+    const index: number = Number(groupName.charAt(length)) - 1;
+    this.group = groupName;
+    this.groupIndex = index;
+
+    this.unreadMessageCount[index] = 0;
+    this.hidden[index] = true;
   }
 }
